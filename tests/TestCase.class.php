1801ae7f6381fdf9e599989da76a4f6a45368bc5
gsmet <gsmet>
2005-11-11 12:03:13 +0000

    no debug for test case

--- tests/TestCase.class.php
+++ tests/TestCase.class.php
@@ -3,6 +3,8 @@
 require_once('simpletest/unit_tester.php');
 require_once('simpletest/reporter.php');
 
+define('DEBUG', 0);
+
 $commonTests = &new GroupTest('Common tests');
 
 $commonTests->addTestFile('TestRegExp.class.php');
