f5a698eaae0e32c3f3c86d8559f753a33ea620a1
gsmet <gsmet>
2005-11-11 12:14:09 +0000

    accumulator refactoring added flushLogStreams

--- include/postgresql/PostgreSQLAccumulator.class.php
+++ include/postgresql/PostgreSQLAccumulator.class.php
@@ -1,15 +1,8 @@
 <?php
 
 class PostgreSQLAccumulator extends Accumulator {
-	var $queries = array();
-	var $errors = array();
-	
-	var $queryListeners = array();
-	var $errorListeners = array();
-	
 	var $working = array();
 	var $stream;
-	var $hasDurationInfo = false;
 	
 	function PostgreSQLAccumulator() {
 		$this->stream = new LogStream();
@@ -29,52 +22,22 @@ class PostgreSQLAccumulator extends Accumulator {
 		}
 	}
 	
-	function addQueryListener(& $queryListener) {
-		$this->queryListeners[] =& $queryListener;
-	}
-	
-	function addErrorListener(& $errorListener) {
-		$this->errorListeners[] =& $errorListener;
-	}
-
-	function appendQuery(& $query) {
-		$this->fireQueryEvent($query);
-	}
-	
-	function appendError(& $error) {
-		$this->fireErrorEvent($error);
-	}
-	
-	function fireQueryEvent(& $query) {
-		$countListeners = count($this->queryListeners);
-		for($i = 0; $i < $countListeners; $i++) {
-			$listener =& $this->queryListeners[$i];
-			$listener->fireEvent($query);
-		}
-	}
-	
-	function fireErrorEvent(& $error) {
-		$countListeners = count($this->errorListeners);
-		for($i = 0; $i < $countListeners; $i++) {
-			$listener =& $this->errorListeners[$i];
-			$listener->fireEvent($error);
-		}
-	}
-	/* TODO : on conserve ou on vire ?
-	function closeOutAll() {
-		$this->hasDurationInfo = $this->stream->hasDurationInfo();
+	function flushLogStreams() {
+		// flush default stream
+		$this->stream->flush($this);
+		$hasDurationInfo = $this->stream->hasDurationInfo();
 		
-		$queries =& $this->stream->getQueries();
-		$queriesCount = count($this->stream->getQueries());
-		
-		@stream.queries.each { |q| q.accumulate_to(self) }
-		@has_duration_info = @stream.has_duration_info
-		@working.each {|k, stream|
-			stream.queries.each { |q| q.accumulate_to(self) }
-			@has_duration_info = @has_duration_info || stream.has_duration_info
+		// flush streams with connection id
+		$logStreamsKeys = array_keys($this->working);
+		foreach($logStreamsKeys AS $key) {
+			$logStream =& $this->working[$key];
+			$logStream->flush($this);
+			$hasDurationInfo = $hasDurationInfo || $logStream->hasDurationInfo();
+			unset($logStream);
 		}
-	}*/
-
+		
+		$this->hasDurationInfo = $hasDurationInfo;
+	}
 }
 
 ?>
