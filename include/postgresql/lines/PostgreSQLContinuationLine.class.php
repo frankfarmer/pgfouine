e2eb3217f2981714e7dab10f4c5567dc72815390
gsmet <gsmet>
2005-11-13 15:26:31 +0000

    added a check on command number (we now add unrecognized line as continuation line as PostgreSQL cuts long lines)

--- include/postgresql/lines/PostgreSQLContinuationLine.class.php
+++ include/postgresql/lines/PostgreSQLContinuationLine.class.php
@@ -9,7 +9,7 @@ class PostgreSQLContinuationLine extends PostgreSQLLogLine {
 
 	function appendTo(& $queries) {
 		$query =& $queries->last();
-		if($query) {
+		if($query && $query->getCommandNumber() == $this->commandNumber) {
 			$query->append($this->text);
 		} else {
 			stderr('Continuation for no previous query');
