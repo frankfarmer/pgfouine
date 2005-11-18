715b41c012e38ce0ad0c47188a927e5c2dbbf6a4
gsmet <gsmet>
2005-11-18 21:16:13 +0000

    not used

--- include/base.lib.php
+++ include/base.lib.php
@@ -5,7 +5,6 @@ require_once('Parser.class.php');
 require_once('Query.class.php');
 require_once('ErrorQuery.class.php');
 require_once('LogStream.class.php');
-require_once('ParseError.class.php');
 require_once('GenericLogReader.class.php');
 require_once('SlowestQueryList.class.php');
 require_once('NormalizedQuery.class.php');
