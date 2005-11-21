c7be25b107f122cbb0f3834bc2ad34655942acf6
gsmet <gsmet>
2005-11-21 23:02:15 +0000

    fixed references to listeners added errors counters

--- include/reporting/reports/OverallStatsReport.class.php
+++ include/reporting/reports/OverallStatsReport.class.php
@@ -6,8 +6,10 @@ class OverallStatsReport extends Report {
 	}
 	
 	function getText() {
-		$listener = $this->reportAggregator->getListener('GlobalCountersListener');
-		$normalizedListener = $this->reportAggregator->getListener('NormalizedQueriesListener');
+		$listener =& $this->reportAggregator->getListener('GlobalCountersListener');
+		$normalizedListener =& $this->reportAggregator->getListener('NormalizedQueriesListener');
+		$errorCountersListener =& $this->reportAggregator->getListener('GlobalErrorCountersListener');
+		$normalizedErrorsListener =& $this->reportAggregator->getListener('NormalizedErrorsListener');
 		
 		$text = '';
 		
@@ -18,13 +20,21 @@ class OverallStatsReport extends Report {
 			'Number of queries:     '.$this->formatInteger($listener->getQueryCount())."\n".
 			'Total query duration:  '.$this->formatLongDuration($listener->getQueryDuration())."\n"
 		;
+		if($errorCountersListener) {
+			$text .= 'Number of errors:     '.$this->formatInteger($errorCountersListener->getErrorCount())."\n";
+			if($normalizedErrorsListener) {
+				$text .= 'Number of unique normalized errors: '.$this->formatInteger($normalizedErrorsListener->getUniqueErrorCount())."\n";
+			}
+		}
 		
 		return $text;
 	}
 	
 	function getHtml() {
-		$listener = $this->reportAggregator->getListener('GlobalCountersListener');
-		$normalizedListener = $this->reportAggregator->getListener('NormalizedQueriesListener');
+		$listener =& $this->reportAggregator->getListener('GlobalCountersListener');
+		$normalizedListener =& $this->reportAggregator->getListener('NormalizedQueriesListener');
+		$errorCountersListener =& $this->reportAggregator->getListener('GlobalErrorCountersListener');
+		$normalizedErrorsListener =& $this->reportAggregator->getListener('NormalizedErrorsListener');
 		
 		$html = '';
 		
@@ -34,6 +44,12 @@ class OverallStatsReport extends Report {
 		}
 		$html .= '<li>Number of queries: '.$this->formatInteger($listener->getQueryCount()).'</li>';
 		$html .= '<li>Total query duration: '.$this->formatLongDuration($listener->getQueryDuration()).'</li>';
+		if($errorCountersListener) {
+			$html .= '<li>Number of errors:     '.$this->formatInteger($errorCountersListener->getErrorCount()).'</li>';
+			if($normalizedErrorsListener) {
+				$html .= '<li>Number of unique normalized errors: '.$this->formatInteger($normalizedErrorsListener->getUniqueErrorCount()).'</li>';
+			}
+		}
 		$html .= '</ul>';
 		
 		return $html;
