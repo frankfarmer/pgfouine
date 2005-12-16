d83aa203fff4ea30d721179987bd2b744682a608
gsmet <gsmet>
2005-12-16 22:55:37 +0000

    fixed help

--- pgfouine.php
+++ pgfouine.php
@@ -18,12 +18,14 @@ function usage($error = false) {
 	if($error) {
 		stderr('Error: '.$error);
 	}
-	echo 'Usage: '.$GLOBALS['executable'].' -file <file> [-top <n>] [-format <format>] [-logtype <logtype>] [-reports report1,report2]
+	echo 'Usage: '.$GLOBALS['executable'].' -file <file> [-top <n>] [-format <format>] [-logtype <logtype>] [-reports <report1,report2>]
   -file <file>                  log file to analyze
   -top <n>                      number of queries in lists. Default is 20.
   -format <format>              output format: html or text. Default is html.
   -logtype <logtype>            log type: only syslog is currently supported
   -reports <report1,report2>    list of reports type separated by a comma
+                                reports can be: overall, bytype, slowest, n-mosttime,
+                                 n-mostfrequent, n-slowestaverage, n-mostfrequenterrors
   -examples <n>                 maximum number of examples for a normalized query
   -onlyselect                   ignore all queries but SELECT
   -debug                        debug mode
