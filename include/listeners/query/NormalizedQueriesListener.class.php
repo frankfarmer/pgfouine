affa810b99100909de915c2565d911522b320242
gsmet <gsmet>
2006-01-10 18:17:42 +0000

    improved the sort of queries

--- include/listeners/query/NormalizedQueriesListener.class.php
+++ include/listeners/query/NormalizedQueriesListener.class.php
@@ -1,26 +1,5 @@
 <?php
 
-/*
- * This file is part of pgFouine.
- * 
- * pgFouine - a PostgreSQL log analyzer
- * Copyright (c) 2005 Guillaume Smet
- *
- * pgFouine is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * pgFouine is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with pgFouine; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
 class NormalizedQueriesListener extends QueryListener {
 	var $queryList = array();
 	var $queriesNumber = 10;
@@ -62,7 +41,7 @@ class NormalizedQueriesListener extends QueryListener {
 	
 	function compareMostFrequent(& $a, & $b) {
 		if($a->getTimesExecuted() == $b->getTimesExecuted()) {
-			return 0;
+			return $this->compareSlowest($a, $b);
 		} elseif($a->getTimesExecuted() < $b->getTimesExecuted()) {
 			return 1;
 		} else {
@@ -78,7 +57,7 @@ class NormalizedQueriesListener extends QueryListener {
 	
 	function compareSlowest(& $a, & $b) {
 		if($a->getAverageDuration() == $b->getAverageDuration()) {
-			return 0;
+			return $this->compareMostFrequent($a, $b);
 		} elseif($a->getAverageDuration() < $b->getAverageDuration()) {
 			return 1;
 		} else {
