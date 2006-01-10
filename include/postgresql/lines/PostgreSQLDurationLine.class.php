d0f3ad92ec4c686bfa544a3eb9f9a487fb5829ab
gsmet <gsmet>
2006-01-10 18:12:37 +0000

    fixed a bug submitted by David Link there was no check on the command number for duration line which mistakenly set duration twice if you use log_duration (this is fixed in the new parser anyway)

--- include/postgresql/lines/PostgreSQLDurationLine.class.php
+++ include/postgresql/lines/PostgreSQLDurationLine.class.php
@@ -1,50 +1,21 @@
 <?php
 
-/*
- * This file is part of pgFouine.
- * 
- * pgFouine - a PostgreSQL log analyzer
- * Copyright (c) 2005 Guillaume Smet
- *
- * pgFouine is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * pgFouine is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with pgFouine; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
 class PostgreSQLDurationLine extends PostgreSQLLogLine {
 	var $ignore = false;
 	
 	function PostgreSQLDurationLine($timeString, $unit) {
-		$this->PostgreSQLLogLine('', $this->parseDuration($timeString, $unit));
+		$this->PostgreSQLLogLine('NO TEXT', $this->parseDuration($timeString, $unit));
 	}
 	
-	function & getLogObject(& $logStream) {
-		if($this->lineNumber == 1) {
-			$durationLogObject = new DurationLogObject($logStream->getUser(), $logStream->getDb(), $this->duration);
-			$durationLogObject->setContextInformation($this->timestamp, $this->commandNumber);
-			return $durationLogObject;
+	function appendTo(& $queries) {
+		$query =& $queries->last();
+		if($query && ($query->getCommandNumber() == $this->commandNumber)) {
+			$query->setDuration($this->duration);
+			return $queries->pop();
 		} else {
 			stderr('Duration for no previous query', true);
 			return false;
 		}
-	}
-	
-	function appendTo(& $logObject) {
-		$logObject->setDuration($this->duration);
-	}
-	
-	function complete() {
-		return true;
 	}
 }
 
