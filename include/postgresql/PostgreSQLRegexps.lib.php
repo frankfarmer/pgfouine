ae10ac713b386cffe56215a1edf818fc620378b8
gsmet <gsmet>
2006-03-23 22:58:37 +0000

    removed an unused regexp added us (microsecond) as a possible time unit

--- include/postgresql/PostgreSQLRegexps.lib.php
+++ include/postgresql/PostgreSQLRegexps.lib.php
@@ -28,10 +28,7 @@ $postgreSQLRegexps['LogLine'] = new RegExp("/^(?:.*?)(LOG|DEBUG|CONTEXT|WARNING|
 $postgreSQLRegexps['ContinuationLine'] = new RegExp("/^(\^I|\s)/");
 $postgreSQLRegexps['QueryStartPart'] = new RegExp("/^(query|statement):[\s]+/");
 $postgreSQLRegexps['StatusPart'] = new RegExp("/^(connection|received|unexpected EOF)/");
-$postgreSQLRegexps['DurationPart'] = new RegExp("/^duration:([\s\d\.]+)(sec|ms)/");
-
-// SyslogPostgreSQLParser
-$postgreSQLRegexps['CommandLine'] = new RegExp('/\[(\d{1,10})(\-\d{1,5}){0,1}\] /');
+$postgreSQLRegexps['DurationPart'] = new RegExp("/^duration:([\s\d\.]+)(sec|ms|us)/");
 
 // PostgreSQLStatusLine
 $postgreSQLRegexps['ConnectionReceived'] = new RegExp('/connection received: host=([^\s]+) port=([\d]+)/');
