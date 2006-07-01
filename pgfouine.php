e561ca3181c9256a186f9314fa17e480a900e62f
gsmet <gsmet>
2006-07-01 09:22:28 +0000

    preparing 0.6.1 release

--- pgfouine.php
+++ pgfouine.php
@@ -22,7 +22,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-define('VERSION', '0.6');
+define('VERSION', '0.6.1');
 
 ini_set('max_execution_time', 18000);
 
